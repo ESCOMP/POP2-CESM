63dbb49a1ab3d1f0d125f7bf78311ced97fc2649
Michael Levy <mlevy@ucar.edu>
2013-12-20 18:35:46 +0000

    POP2 fix for intel compiler
    git-svn-id: https://svn-ccsm-models.cgd.ucar.edu/pop2/trunk@56176 fe37f545-8307-0410-aea5-b40df96820b5

--- source/cfc11_mod.F90
+++ source/cfc11_mod.F90
@@ -442,7 +442,7 @@ contains
     !---------------------------------------------------------------------------
 
     allocate( LAND_MASK(nx_block,ny_block,max_blocks_clinic) )
-    LAND_MASK = merge(.true., .false., KMT > 0)
+    LAND_MASK = (KMT.gt.0)
 
     call get_timer(cfc11_sflux_timer, 'CFC11_SFLUX',1, &
                                           distrb_clinic%nprocs)
