ee1af671c3d3f611a730bb6d32ff9e70c857f644
Michael Levy <mlevy@ucar.edu>
2017-06-14 19:38:02 +0000

    revert marbl_dev_levy back to marbl_dev
    git-svn-id: https://svn-ccsm-models.cgd.ucar.edu/pop2/branches/marbl_dev_levy@85678 fe37f545-8307-0410-aea5-b40df96820b5

--- source/ecosys_tavg.F90
+++ source/ecosys_tavg.F90
@@ -66,6 +66,8 @@ contains
     ! !DESCRIPTION:
     !  call define_tavg_field for all tavg fields
 
+    use ecosys_tracers_and_saved_state_mod, only : marbl_tracer_cnt
+
     implicit none
 
     type(marbl_interface_class)  , intent(in) :: marbl_instance
@@ -213,7 +215,8 @@ contains
 
     ! Accumulate diagnostics for surface fluxes
 
-    use marbl_diagnostics_mod, only : ind => marbl_surface_forcing_diag_ind
+    use ecosys_tracers_and_saved_state_mod, only : marbl_tracer_cnt
+    use marbl_diagnostics_mod,              only : ind => marbl_surface_forcing_diag_ind
 
     implicit none
 
