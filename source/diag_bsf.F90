507a03f44ff0df7bea87b1f40dedb13ce010ceb4
alperaltuntas <alperaltuntas@gmail.com>
2019-11-04 14:25:04 -0700

    check for undefined_nf_int instead of <0

--- source/diag_bsf.F90
+++ source/diag_bsf.F90
@@ -885,9 +885,9 @@
 
    call gather_global (WORK1, KMT, master_task,distrb_clinic)
 
-   ! Set WORK1 (i.e., KMT) to zero where negative. (Eliminated land blocks get filled
-   ! with undefined_nf_int, but the below code block assumes KMT >= 0.)
-   where (WORK1<0) WORK1 = 0
+   ! Set WORK1 (i.e., KMT) to zero in eliminated land blocks, since
+   ! below code block expects land cells to be KMT=0.
+   where (WORK1.eq.undefined_nf_int) WORK1 = 0
 
    error_code = 0
 
